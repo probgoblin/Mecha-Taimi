23cd75e71b2f66e7ff7fd23f5e830fbde85774ea
Jeremy D. Thralls <thralls@gmx.net>
2019-02-19 23:04:39 +0100

    revert previous commit

--- src/main/java/me/cbitler/raidbot/raids/Raid.java
+++ src/main/java/me/cbitler/raidbot/raids/Raid.java
@@ -498,9 +498,7 @@ public class Raid {
             List<RaidUser> raidUsersInRole = getUsersInRole(role.name);
             text += ("**" + role.name + " ( " + raidUsersInRole.size() + " / " + role.amount + " ):** \n");
             for (RaidUser user : raidUsersInRole) {
-                System.out.println(user.spec);
                 Emote userEmote = Reactions.getEmoteByName(user.spec);
-                System.out.println(userEmote);
                 text += "   <:"+userEmote.getName()+":"+userEmote.getId()+"> " + user.name + " (" + user.spec + ")\n";
             }
             text += "\n";
