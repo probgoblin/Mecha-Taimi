f1b6b63694c369112dc99c5ebcd462cda92e9ace
Franziska Müller <franziska7993@t-online.de>
2019-03-06 09:15:00 +0100

    equals and hashCode methods in RaidUser to ensure flex roles are counted correctly per user. Earlier the key in the flex role map was the corresponding RaidUser instance in the main role map but we want to be able to add flex roles for users without main role. Hence, flex roles are added with a RaidUser key with empty spec and role.

--- src/main/java/me/cbitler/raidbot/raids/RaidUser.java
+++ src/main/java/me/cbitler/raidbot/raids/RaidUser.java
@@ -17,6 +17,37 @@ public class RaidUser {
         this.spec = spec;
         this.role = role;
     }
+    
+    /**
+     * the equals method
+     * @param other the object to compare to
+     * @return true if all members are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object other) {
+    	if (other instanceof RaidUser) {
+    		RaidUser otherRole = (RaidUser) other;
+    		if (this.id.equals(otherRole.id)
+    			&& this.name.equals(otherRole.name)
+    			&& this.spec.equals(otherRole.spec)
+    			&& this.role.equals(otherRole.role)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    /**
+     * the hashCode method
+     * @return true if all members are equal, false otherwise
+     */
+    @Override
+    public int hashCode() {
+    	return this.id.hashCode()
+    			+ this.name.hashCode()
+    			+ this.role.hashCode()
+    			+ this.spec.hashCode();
+    }
 
     public String getId() {
         return id;
