6755b6131b60ff13ce0374dc717196437be62bcc
Franziska Müller <franziska7993@t-online.de>
2019-03-09 16:11:24 +0100

    default channels for announcement

--- src/main/java/me/cbitler/raidbot/creation/RunChannelStep.java
+++ src/main/java/me/cbitler/raidbot/creation/RunChannelStep.java
@@ -10,41 +10,84 @@ import net.dv8tion.jda.core.events.message.priv.PrivateMessageReceivedEvent;
  * @author Christopher Bitler
  */
 public class RunChannelStep implements CreationStep {
-    /**
+    static String[] defaultChannels = { "board-of-adventures", "gw2-raid-bot", "test" };
+	boolean enterManually;
+    
+	public RunChannelStep() {
+		this.enterManually = false;
+	}
+	
+	/**
      * Set the announcement channel
      * @param e The direct message event
      * @return true if the announcement channel was set, false if it was not
      */
     public boolean handleDM(PrivateMessageReceivedEvent e) {
-        RaidBot bot = RaidBot.getInstance();
-        PendingRaid raid = bot.getPendingRaids().get(e.getAuthor().getId());
-        if (raid == null) {
-            return false;
+        if (enterManually) {
+            String channelWithoutHash = e.getMessage().getRawContent().replace("#","");
+        	if (checkAndSetChannel(e.getAuthor().getId(), channelWithoutHash) == false) {
+				e.getChannel().sendMessage("Please choose a valid channel.").queue();
+				return false;
+			}
+			return true;
+        } else {
+        	try {
+        		int choiceId = Integer.parseInt(e.getMessage().getRawContent()) - 1;
+        		if (choiceId >= 0 && choiceId < defaultChannels.length) { // one of the default channels
+        			if (checkAndSetChannel(e.getAuthor().getId(), defaultChannels[choiceId]) == false) {
+        				e.getChannel().sendMessage("Please choose a valid channel.").queue();
+        				return false;
+        			}
+        			return true;
+        		} else if (choiceId == defaultChannels.length) { // user wants to enter name manually
+        			enterManually = true;
+        			e.getChannel().sendMessage("Enter the channel for event announcement:").queue();
+        			return false;
+        		} else { // no valid choice
+        			e.getChannel().sendMessage("Please choose a valid option.").queue();
+        			return false;
+        		}   	
+        	} catch (Exception excp) { // not an integer
+        		e.getChannel().sendMessage("Please choose a valid option.").queue();
+        		return false;
+        	}
         }
-
-        String channelWithoutHash = e.getMessage().getRawContent().replace("#","");
-        boolean validChannel = false;
+    }
+    
+    /** 
+     * checks if a given channel exists and if yes, sets this channel as announcement channel
+     * @param authorId the user creating the current raid
+     * @param channelName the channel name chosen by the user
+     * @return true if channel is valid, false otherwise
+     */
+    private boolean checkAndSetChannel(String authorId, String channelName) {
+    	RaidBot bot = RaidBot.getInstance();
+    	PendingRaid raid = bot.getPendingRaids().get(authorId);
+    	if (raid == null)
+    		return false;
+        
+    	boolean validChannel = false;
         for (TextChannel channel : bot.getServer(raid.getServerId()).getTextChannels()) {
-            if(channel.getName().replace("#","").equalsIgnoreCase(channelWithoutHash)) {
+            if(channel.getName().replace("#","").equalsIgnoreCase(channelName)) {
                 validChannel = true;
             }
         }
+        
+        if (validChannel)
+        	raid.setAnnouncementChannel(channelName);
 
-        if(!validChannel) {
-            e.getChannel().sendMessage("Please choose a valid channel.").queue();
-            return false;
-        }
-
-        raid.setAnnouncementChannel(e.getMessage().getRawContent().replace("#",""));
-
-        return true;
+        return validChannel;
     }
 
     /**
      * {@inheritDoc}
      */
     public String getStepText() {
-        return "Enter the channel for event announcement:";
+        String text = "Choose the channel for event announcement:\n";
+        for (int c = 0; c < defaultChannels.length; c++)
+        	text += "`" + (c+1) + "` *" + defaultChannels[c] + "*\n";
+        text += "`" + (defaultChannels.length+1) + "` enter name manually";
+        return text;
     }
 
     /**
