8d642422929bd78b7d21af0d462e035097d34931
Franziska Müller <franziska7993@t-online.de>
2019-07-15 11:42:14 +0200

    added end all command file

---
+++ src/main/java/me/cbitler/raidbot/commands/EndAllCommand.java
@@ -0,0 +1,37 @@
+package me.cbitler.raidbot.commands;
+
+import me.cbitler.raidbot.raids.Raid;
+import me.cbitler.raidbot.raids.RaidManager;
+import me.cbitler.raidbot.utility.PermissionsUtil;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EndAllCommand implements Command {
+    @Override
+    public void handleCommand(String command, String[] args, TextChannel channel, User author) {
+        Member member = channel.getGuild().getMember(author);
+        if (PermissionsUtil.hasRaidLeaderRole(member)) {
+            List<Raid> allEvents = RaidManager.getAllRaids();
+            int numEvents = allEvents.size();
+            List<String> notDeleted = new ArrayList<String>();
+            for (int ev = 0; ev < numEvents; ev++) {
+            	Raid raid = allEvents.get(ev);
+                if (raid != null && raid.getServerId().equalsIgnoreCase(channel.getGuild().getId())) {
+                	String name = raid.getName();
+                    boolean deleted = RaidManager.deleteRaid(raid.getMessageId());
+                    if (deleted == false) {
+                    	notDeleted.add(name);
+                    }
+                }
+            }
+            author.openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage("Successfully ended " + (numEvents - notDeleted.size()) + " of " + numEvents + " existing events.").queue());
+            for (String eventName : notDeleted) {
+            	author.openPrivateChannel().queue(privateChannel -> privateChannel.sendMessage("Could not end event: " + eventName).queue());
+            }
+        }
+    }
+}
