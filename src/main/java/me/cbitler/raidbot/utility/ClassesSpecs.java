1d18e6ece72f88ec15cd0a325e209b5f6152a5b0
JDThralls <j.thralls@pm.me>
2021-11-09 02:48:57 +0100

    prepare class specs for the update

--- src/main/java/me/cbitler/raidbot/utility/ClassesSpecs.java
+++ src/main/java/me/cbitler/raidbot/utility/ClassesSpecs.java
@@ -7,33 +7,60 @@ public class ClassesSpecs {
      * List of classes and their specializations
      */
     static String[][] classesSpecs = {
-    		{	"Guardian",
-    			"Dragonhunter", // 547490713764364329
-            	"Firebrand"	}, // 547490713928204308
-    		{	"Revenant",
-            	"Herald", // 547490714049708032
-            	"Renegade" }, // 547490713584140323
-    		{	"Warrior",
-            	"Berserker", // 547490714041450496
-            	"Spellbreaker" }, // 547490713990987806
-    		{	"Engineer",
-            	"Scrapper", // 547490714079068184
-            	"Holosmith" }, // 547490713911296040
-    		{	"Ranger",
-            	"Druid", // 547490713894518792
-            	"Soulbeast" }, // 547490714041319439
-    		{	"Thief",
-            	"Daredevil", // 547490713927942169
-            	"Deadeye" }, // 547490714209222676
-    		{ 	"Elementalist",
-            	"Weaver", // 547490714007764994
-            	"Tempest" }, // 547490714045644810
-    		{ 	"Mesmer",
-            	"Chronomancer", // 547490713567232021
-            	"Mirage" }, // 547490714624458752
-    		{ 	"Necromancer",
-            	"Reaper", // 547490713642729478
-            	"Scourge" } // 547490713965690897
+            {
+                "Guardian",
+                "Dragonhunter",
+                "Firebrand",
+                "Willbender"
+            },
+            {
+                "Revenant",
+                "Herald",
+                "Renegade",
+                "Vindicator"
+            },
+            {
+                "Warrior",
+                "Berserker",
+                "Spellbreaker",
+                "Bladesworn"
+            },
+            {
+                "Engineer",
+                "Scrapper",
+                "Holosmith",
+                "Mechanist"
+            },
+            {
+                "Ranger",
+                "Druid",
+                "Soulbeast",
+                "Untamed"
+            },
+            {
+                "Thief",
+                "Daredevil",
+                "Deadeye",
+                "Specter"
+            },
+            {
+                "Elementalist",
+                "Weaver",
+                "Tempest",
+                "Catalyst"
+            },
+            {
+                "Mesmer",
+                "Chronomancer",
+                "Mirage",
+                "Virtuoso"
+            },
+            {
+                "Necromancer",
+                "Reaper",
+                "Scourge",
+                "Harbinger"
+            }
     };
 
     static HashMap<String, Integer> coreClassIds = new HashMap<String, Integer>() {{
@@ -56,12 +83,12 @@ public class ClassesSpecs {
      * @return The array of available specializations for this core class
      */
     public static String[] getSpecsForCore(String coreclass) {
-    	Integer classId = coreClassIds.get(coreclass);
-    	if (classId != null) {
-    		return classesSpecs[classId];
-    	}
-    	else {
-    		return new String[0];
-    	}
+        Integer classId = coreClassIds.get(coreclass);
+        if (classId != null) {
+            return classesSpecs[classId];
+        }
+        else {
+            return new String[0];
+        }
     }
 }
--- src/main/java/me/cbitler/raidbot/utility/Reactions.java
+++ src/main/java/me/cbitler/raidbot/utility/Reactions.java
@@ -12,102 +12,112 @@ public class Reactions {
      * List of reactions representing classes
      */
     static String[] specs = {
-            "Dragonhunter", // 530541439180996638
-            "Firebrand", // 530541440300744705
-            "Herald", // 530541440086966283
-            "Renegade", // 530541439478661120
-            "Berserker", // 530541439625592852
-            "Spellbreaker", // 530541439675793408
-            "Scrapper", // 530541438895652914
-            "Holosmith", // 530541438837194753
-            "Druid", // 530541439105499136
-            "Soulbeast", // 530541439415746585
-            "Daredevil", // 530541439705415680
-            "Deadeye", // 530541440225509377
-            "Weaver", // 530541439239716874
-            "Tempest", // 530541439252430858
-            "Chronomancer", // 530541439621267456
-            "Mirage", // 530541438707040277
-            "Reaper", // 530541439038259216
-            "Scourge", // 530541439772393484
-            "Guardian", // 530541438891589643
-            "Revenant", // 530541439751290901
-            "Warrior", // 530541440145555466
-            "Engineer", // 530541439172608062
-            "Ranger", // 530541439642501121
-            "Thief", // 530541439701221378
-            "Elementalist", // 530541438891589642
-            "Mesmer", // 530541438769823747
-            "Necromancer" // 530541439218876416
+            "Dragonhunter",
+            "Firebrand",
+            "Herald",
+            "Renegade",
+            "Berserker",
+            "Spellbreaker",
+            "Scrapper",
+            "Holosmith",
+            "Druid",
+            "Soulbeast",
+            "Daredevil",
+            "Deadeye",
+            "Weaver",
+            "Tempest",
+            "Chronomancer",
+            "Mirage",
+            "Reaper",
+            "Scourge",
+            "Guardian",
+            "Revenant",
+            "Warrior",
+            "Engineer",
+            "Ranger",
+            "Thief",
+            "Elementalist",
+            "Mesmer",
+            "Necromancer"
     };
 
     public static String[] coreClasses = {
-            "Guardian", // 530541438891589643
-            "Revenant", // 530541439751290901
-            "Warrior", // 530541440145555466
-            "Engineer", // 530541439172608062
-            "Ranger", // 530541439642501121
-            "Thief", // 530541439701221378
-            "Elementalist", // 530541438891589642
-            "Mesmer", // 530541438769823747
-            "Necromancer" // 530541439218876416
+            "Guardian",
+            "Revenant",
+            "Warrior",
+            "Engineer",
+            "Ranger",
+            "Thief",
+            "Elementalist",
+            "Mesmer",
+            "Necromancer"
     };
 
     static Emote[] reactions = {
-            getEmoji("530541439180996638"), // Dragonhunter
-            getEmoji("530541440300744705"), // Firebrand
-            getEmoji("530541440086966283"), // Herald
-            getEmoji("530541439478661120"), // Renegade
-            getEmoji("530541439625592852"), // Berserker
-            getEmoji("530541439675793408"), // Spellbreaker
-            getEmoji("530541438895652914"), // Scrapper
-            getEmoji("530541438837194753"), // Holosmith
-            getEmoji("530541439105499136"), // Druid
-            getEmoji("530541439415746585"), // Soulbeast
-            getEmoji("530541439705415680"), // Daredevil
-            getEmoji("530541440225509377"), // Deadeye
-            getEmoji("530541439239716874"), // Weaver
-            getEmoji("530541439252430858"), // Tempest
-            getEmoji("530541439621267456"), // Chronomancer
-            getEmoji("530541438707040277"), // Mirage
-            getEmoji("530541439038259216"), // Reaper
-            getEmoji("530541439772393484"), // Scourge
-            getEmoji("530541438891589643"), // Guardian
-            getEmoji("530541439751290901"), // Revenant
-            getEmoji("530541440145555466"), // Warrior
-            getEmoji("530541439172608062"), // Engineer
-            getEmoji("530541439642501121"), // Ranger
-            getEmoji("530541439701221378"), // Thief
-            getEmoji("530541438891589642"), // Elementalist
-            getEmoji("530541438769823747"), // Mesmer
-            getEmoji("530541439218876416"), // Necromancer
-            getEmoji("592463347291783168"), // Flex
-            getEmoji("614289684754857984"), // Swap
-            getEmoji("548530947591634944"), // X_
-            getEmoji("727111299749904396")  // edit
+            getEmojiFromEnvVar("EMOTE_DRAGONHUNTER"),
+            getEmojiFromEnvVar("EMOTE_FIREBRAND"),
+            getEmojiFromEnvVar("EMOTE_HERALD"),
+            getEmojiFromEnvVar("EMOTE_RENEGADE"),
+            getEmojiFromEnvVar("EMOTE_BERSERKER"),
+            getEmojiFromEnvVar("EMOTE_SPELLBREAKER"),
+            getEmojiFromEnvVar("EMOTE_SCRAPPER"),
+            getEmojiFromEnvVar("EMOTE_HOLOSMITH"),
+            getEmojiFromEnvVar("EMOTE_DRUID"),
+            getEmojiFromEnvVar("EMOTE_SOULBEAST"),
+            getEmojiFromEnvVar("EMOTE_DAREDEVIL"),
+            getEmojiFromEnvVar("EMOTE_DEADEYE"),
+            getEmojiFromEnvVar("EMOTE_WEAVER"),
+            getEmojiFromEnvVar("EMOTE_TEMPEST"),
+            getEmojiFromEnvVar("EMOTE_CHRONOMANCER"),
+            getEmojiFromEnvVar("EMOTE_MIRAGE"),
+            getEmojiFromEnvVar("EMOTE_REAPER"),
+            getEmojiFromEnvVar("EMOTE_SCOURGE"),
+            getEmojiFromEnvVar("EMOTE_GUARDIAN"),
+            getEmojiFromEnvVar("EMOTE_REVENANT"),
+            getEmojiFromEnvVar("EMOTE_WARRIOR"),
+            getEmojiFromEnvVar("EMOTE_ENGINEER"),
+            getEmojiFromEnvVar("EMOTE_RANGER"),
+            getEmojiFromEnvVar("EMOTE_THIEF"),
+            getEmojiFromEnvVar("EMOTE_ELEMENTALIST"),
+            getEmojiFromEnvVar("EMOTE_MESMER"),
+            getEmojiFromEnvVar("EMOTE_NECROMANCER"),
+            getEmojiFromEnvVar("EMOTE_FLEX"),
+            getEmojiFromEnvVar("EMOTE_SWAP"),
+            getEmojiFromEnvVar("EMOTE_CANCEL"),
+            getEmojiFromEnvVar("EMOTE_EDIT")
     };
 
     static Emote[] reactionsCore = {
-            getEmoji("530541438891589643"), // Guardian
-            getEmoji("530541439751290901"), // Revenant
-            getEmoji("530541440145555466"), // Warrior
-            getEmoji("530541439172608062"), // Engineer
-            getEmoji("530541439642501121"), // Ranger
-            getEmoji("530541439701221378"), // Thief
-            getEmoji("530541438891589642"), // Elementalist
-            getEmoji("530541438769823747"), // Mesmer
-            getEmoji("530541439218876416"), // Necromancer
-            getEmoji("592463347291783168"), // Flex
-            getEmoji("614289684754857984"), // Swap
-            getEmoji("548530947591634944"), // X_
-            getEmoji("727111299749904396")  // edit
+            getEmojiFromEnvVar("EMOTE_GUARDIAN"),
+            getEmojiFromEnvVar("EMOTE_REVENANT"),
+            getEmojiFromEnvVar("EMOTE_WARRIOR"),
+            getEmojiFromEnvVar("EMOTE_ENGINEER"),
+            getEmojiFromEnvVar("EMOTE_RANGER"),
+            getEmojiFromEnvVar("EMOTE_THIEF"),
+            getEmojiFromEnvVar("EMOTE_ELEMENTALIST"),
+            getEmojiFromEnvVar("EMOTE_MESMER"),
+            getEmojiFromEnvVar("EMOTE_NECROMANCER"),
+            getEmojiFromEnvVar("EMOTE_FLEX"),
+            getEmojiFromEnvVar("EMOTE_SWAP"),
+            getEmojiFromEnvVar("EMOTE_CANCEL"),
+            getEmojiFromEnvVar("EMOTE_EDIT")
     };
 
     static Emote[] reactionsOpenWorld = {
-            getEmoji("553296724584562688"), // Check
-            getEmoji("548530947591634944"), // X_
-            getEmoji("727111299749904396")  // edit
+            getEmojiFromEnvVar("EMOTE_CHECK"),
+            getEmojiFromEnvVar("EMOTE_CANCEL"),
+            getEmojiFromEnvVar("EMOTE_EDIT")
     };
+
+    /**
+     * Get an emoji from it's emote ID via JDA
+     *
+     * @param id The ID of the emoji
+     * @return The emote object representing that emoji
+     */
+    private static Emote getEmojiFromEnvVar(String varName) {
+        return getEmoji(EnvVariables.getValue(varName));
+    }
 
     /**
      * Get an emoji from it's emote ID via JDA
